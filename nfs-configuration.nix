b47dd6aae823efd3c12fe68da38b222169b560e7
ikygoose <isaac.p.weigel@gmail.com>
2023-11-30 14:44:06 -0600

    working vaultwarden setup with postgresql and nfs share

--- nfs-configuration.nix
+++ nfs-configuration.nix
@@ -6,8 +6,21 @@
     type = lib.types.str;
   };
 
+  options.nfs-configuration.storageUsers = lib.mkOption
+  {
+    type = lib.types.listOf lib.types.str;
+  };
+
   config.fileSystems."/mnt/nfs" = {
     device = "storage.server.techsupporter.net:/srv/nfs/storage/vm/${config.nfs-configuration.path}";
     fsType = "nfs";
   };
+
+  # group needed for accessing data on the nfs share
+  config.users.groups.vmstorage =
+  {
+    gid = 1011;
+  };
+
+  config.users.groups.vmstorage.members = config.nfs-configuration.storageUsers;
 }
--- vault/configuration.nix
+++ vault/configuration.nix
@@ -15,11 +15,80 @@
       ./nfs-configuration.nix
     ];
 
-  server-configuration.address = "10.0.0.3";
+  config.server-configuration.address = "10.0.0.3";
 
-  networking.hostName = "vault";
+  config.networking.hostName = "vault";
+  config.networking.firewall.allowedTCPPorts = [ 80 ];
 
-  nfs-configuration.path = "vault";
+  config.nfs-configuration.path = "vault";
+  config.nfs-configuration.storageUsers = 
+  [
+    "vaultwarden"
+    "postgres"
+  ];
+
+  config.environment.systemPackages = with pkgs; [
+    vaultwarden-postgresql
+    postgresql
+  ];
+
+  config.services.vaultwarden = 
+  {
+    enable = true;
+    dbBackend = "postgresql";
+    config = 
+    {
+      DATA_FOLDER = "/mnt/nfs/vaultwarden/data";
+      DATABASE_URL = "postgresql://vaultwarden:${builtins.readFile "/secrets/passwords/postgresql/vaultwarden.txt"}@127.0.0.1:5432/vaultwarden";
+      WEB_VAULT_ENABLED = true;
+
+      DOMAIN = "https://password.techsupporter.net";
+
+      ROCKET_ADDRESS = "0.0.0.0";
+      ROCKET_PORT = 80;  # Defaults to 80 in the Docker images, or 8000 otherwise.
+      ROCKET_WORKERS = 10;
+      #ROCKET_TLS={certs="/path/to/certs.pem",key="/path/to/key.pem"};
+
+      SMTP_HOST = "mail.techsupporter.net";
+      SMTP_FROM = "no-reply@techsupporter.net";
+      SMTP_FROM_NAME = "VaultWarden";
+      SMTP_SECURITY = "starttls"; # ("starttls", "force_tls", "off") Enable a secure connection. Default is "starttls" (Explicit - ports 587 or 25), "force_tls" (Implicit - port 465) or "off", no encryption (port 25)
+      SMTP_PORT = 587;          # Ports 587 (submission) and 25 (smtp) are standard without encryption and with encryption via STARTTLS (Explicit TLS). Port 465 (submissions) is used for encrypted submission (Implicit TLS).
+      SMTP_USERNAME = "no-reply@techsupporter.net";e
+      SMTP_PASSWORD = (builtins.readFile "/secrets/passwords/vaultwarden/email.txt");
+      SMTP_TIMEOUT = 15;
+    
+      ADMIN_TOKEN = (builtins.readFile "/secrets/passwords/vaultwarden/admin.txt");
+    };
+  };
+
+  config.services.postgresql = 
+  {
+    enable = true;
+
+    package = pkgs.postgresql_14;
+
+    enableTCPIP = true;
+    port = 5432;
+
+    dataDir = "/mnt/nfs/postgresql/${config.services.postgresql.package.psqlSchema}";
+    ensureDatabases = [ "vaultwarden" ];
+
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser origin-address auth-method
+      local all      all                    trust
+      # ipv4
+      host  all      all     127.0.0.1/32   trust
+      # ipv6
+      host all       all     ::1/128        trust
+    '';
+
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE vaultwarden WITH LOGIN PASSWORD '${builtins.readFile "/secrets/passwords/postgresql/vaultwarden.txt"}' CREATEDB;
+      CREATE DATABASE vaultwarden;
+      GRANT ALL PRIVILEGES ON DATABASE vaultwarden TO vaultwarden;
+    '';
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
@@ -32,5 +101,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  config.system.stateVersion = "23.05"; # Did you read the comment?
 }
